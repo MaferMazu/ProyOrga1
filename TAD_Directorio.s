5b984619070658c841990fa49ac8e0bf765db1b5
Mafer Mazu <13-10787@usb.ve>
2018-02-12 01:35:01 -0400

    syscalls

--- TAD_Directorio.s
+++ TAD_Directorio.s
@@ -12,12 +12,15 @@ dir_cp:
 #Abro archivo nombre1
 la $a0 nombre1
 addi $a1, $zero, 0 #Abro con solo lectura
+addi $v0,$zero,13 #Llamada para abrir
 syscall
 #Tengo que comprobar si es negativo
 #Si es positivo
 lw $a0,($v0) #Coloco en a0 el descriptor
 la $a1 #Coloco en a1 la direccion de el input buffer
 addi $a2,$zero,10000 #La cantidad maxima de caracteres
+addi $v0, $zero, 14 # Llamada para leer
+syscall
 
 perror:
 #a0 tengo el valor a comprobar
