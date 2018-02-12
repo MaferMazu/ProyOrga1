daf167748d139c6cae0dad43ea4a48a38062e799
Mafer Mazu <13-10787@usb.ve>
2018-02-12 01:29:31 -0400

    inicio de todo

---
+++ TAD_Directorio.s
@@ -0,0 +1,23 @@
+.data
+nombre1: .asciiz ""
+.text
+dir_init:
+#Guardar una dir en v0
+dir_cp:
+#Copia el contenido del archivo nombre1 al archivo nuevo nombre2.
+#a0 String .asciiz nombre1
+#a1 String nombre2
+#Guardo en v0 salida
+
+#Abro archivo nombre1
+la $a0 nombre1
+addi $a1, $zero, 0 #Abro con solo lectura
+syscall
+#Tengo que comprobar si es negativo
+#Si es positivo
+lw $a0,($v0) #Coloco en a0 el descriptor
+la $a1 #Coloco en a1 la direccion de el input buffer
+addi $a2,$zero,10000 #La cantidad maxima de caracteres
+
+perror:
+#a0 tengo el valor a comprobar
