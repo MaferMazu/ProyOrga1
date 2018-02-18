924831e5d39e1e2483d926c3678ddfb9826a9486
Mafer Mazu <13-10787@usb.ve>
2018-02-18 00:02:20 -0400

    :D

--- TAD_Directorio.s
+++ TAD_Directorio.s
@@ -1,26 +1,33 @@
 .data
-nombre1: .asciiz ""
+Directorio: .word 0
 .text
 dir_init:
-#Guardar una dir en v0
+	#Prologo
+	sw $fp,($sp)
+	move $fp,$sp
+	addi $sp,$sp,-4
+	li $v0,9
+	li $a0,12
+	syscall
+	sw $v0, Directorio
+	#Epilogo
+	move $sp,$fp
+	lw $fp,($sp)
+	jr $ra
+
 dir_cp:
-#Copia el contenido del archivo nombre1 al archivo nuevo nombre2.
-#a0 String .asciiz nombre1
-#a1 String nombre2
-#Guardo en v0 salida
+	#Prologo
+	sw $fp,($sp)
+	move $fp,$sp
+	addi $sp,$sp,-4
 
-#Abro archivo nombre1
-la $a0 nombre1
-addi $a1, $zero, 0 #Abro con solo lectura
-addi $v0,$zero,13 #Llamada para abrir
-syscall
-#Tengo que comprobar si es negativo
-#Si es positivo
-lw $a0,($v0) #Coloco en a0 el descriptor
-la $a1 #Coloco en a1 la direccion de el input buffer
-addi $a2,$zero,10000 #La cantidad maxima de caracteres
-addi $v0, $zero, 14 # Llamada para leer
-syscall
-
-perror:
-#a0 tengo el valor a comprobar
+buscar_archivo:
+	#Prologo
+	sw $fp,($sp)
+	move $fp,$sp
+	sw $ra, -4($fp)
+	addi $sp,$sp,-8
+	move $s0,$a0
+	lw $t0,4(Directorio)
+loop_buscar_archivo:
+	beqz $t0, buscar_fin
